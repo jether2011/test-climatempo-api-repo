6419679abaf6fb0ff191c3c6423f2eba4c96f1a1
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2017-11-21 01:44:53 -0200

    add select ion to choose a state

--- src/providers/cities-service/cities-service.ts
+++ src/providers/cities-service/cities-service.ts
@@ -41,11 +41,7 @@ export class CitiesServiceProvider {
     }
     console.log(state);
     return new Promise(resolve => {
-<<<<<<< HEAD
       this.http.get(this.urlCities + `${state}`)
-=======
-      this.http.get(this.urlCities)   
->>>>>>> d90b826cc30fa07b65dc07077560d148b6057e5c
         //.do((response : Response ) => console.log(response.json()))
         //.map((response : Response ) => response.json())     
         .subscribe(data => {
